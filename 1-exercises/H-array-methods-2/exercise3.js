6d6dbd91792f2df99c4fa4bd567b0c04521944e9
JarrodBen <46967203+JarrodBen@users.noreply.github.com>
2021-02-02 16:01:33 +0200

    did 3rd exercise of H folder

--- 1-exercises/H-array-methods-2/exercise3.js
+++ 1-exercises/H-array-methods-2/exercise3.js
@@ -7,7 +7,8 @@
 var ukNations = ["Scotland", "Wales", "England", "Northern Ireland"];
 
 function isInUK(country) {
-  return; // complete this statement
+  return ukNations.includes(country); // complete this statement
+  // will return either true or false depending on the array that is called.
 }
 
 /* 
