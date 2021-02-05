19819bcb9053d74456c0a3c9e54eb50be7983458
JarrodBen <46967203+JarrodBen@users.noreply.github.com>
2021-02-05 12:34:08 +0200

    made changes to suit requirements

--- 1-exercises/E-array-map/exercise.js
+++ 1-exercises/E-array-map/exercise.js
@@ -2,12 +2,22 @@
 // Write multiple solutions using different syntax (as shown in the README)
 
 
+var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 
 function timesNum100(numbers) {
     return numbers * 100 // function will take a number and multiply it by 100 
 }
+let numbers1 = numbers.map(timesNum100); // Method 1 will call the "timesNum100" function and use it to multiply each number by 100.
 
-var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
-var numbersTimes100 = numbers.map(timesNum100); // will call the "timesNum100" function and use it to multiply each number by 100.
 
-console.log(numbersTimes100);
+
+let numbers2 = numbers.map(function (numbers) { // Method 2 will refer to the function but not to it's name as it is just being used in this code as is.
+    return numbers * 100; 
+}); 
+
+
+let numbers3 = numbers.map((number) => number *100); // Method 3 will use arrow notation to refer to the declared function.
+
+console.log(numbers1);
+console.log(numbers2);
+console.log(numbers3);
